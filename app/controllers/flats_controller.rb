75448db9575fe2d25319de04b615ed15fe488729
Manuel Sousa <mlrcbsousa@gmail.com>
2018-11-28 18:05:29 +0000

    index view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,44 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flats_path
+
+    # @restaurant = Restaurant.new(restaurant_params)
+    # if @restaurant.save
+    #   @restaurant.save
+    #   redirect_to restaurant_path(@restaurant)
+    # else
+    #   render :new
+    # end
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
