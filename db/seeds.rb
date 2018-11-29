1e98c85fc11cca89c53f228db12e57d9de8631f8
Manuel Sousa <mlrcbsousa@gmail.com>
2018-11-29 00:05:49 +0000

    show view added

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ puts 'Cleaning database...'
 Flat.destroy_all
 
 puts 'Creating flats...'
-20.times do
+50.times do
   Flat.create!(
     name: Faker::Lorem.sentence,
     address: Faker::Address.street_address,
     description: Faker::Lorem.paragraph_by_chars(255),
     price_per_night: rand(50..150),
     number_of_guests: rand(1..5),
-    picture_url: 'https://source.unsplash.com/collection/1536566'
+    picture_url: "https://picsum.photos/1200/800?image=#{rand(1084)}"
   )
 end
 puts 'Finished!'
